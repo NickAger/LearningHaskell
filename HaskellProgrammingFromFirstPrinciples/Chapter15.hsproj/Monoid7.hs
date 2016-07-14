22a73bd20ce6388e22e8af330eaeea0fa04ccf16
Nick Ager <nick.ager@gmail.com>
2016-07-14 11:11:18 +0100

    Chapter15 complete!

--- HaskellProgrammingFromFirstPrinciples/Chapter15.hsproj/Monoid7.hs
+++ HaskellProgrammingFromFirstPrinciples/Chapter15.hsproj/Monoid7.hs
@@ -1,15 +1,19 @@
-module SemiGroup10 where
+module Monoid7 where
   
 import Data.Semigroup
 import Test.QuickCheck
 import SemiGroupAssociativeLaw
 import Test.HUnit
+import MonoidLaws
 
 newtype Comp a = Comp { unComp :: (a -> a) }
 
-
 instance Semigroup (Comp a) where
     f1 <> f2 = Comp $ (unComp f1) . (unComp f2)
+    
+instance Monoid (Comp a)  where
+  mempty = Comp id
+  mappend = (<>)
     
 -- think this link could help: http://stackoverflow.com/questions/16214093/how-to-generate-random-typed-functions
 --instance (Coarbitrary a, Arbitrary b) => Arbitrary (a -> b) where
@@ -22,12 +26,15 @@ test1 = TestCase (assertEqual ""  (unComp (f <> g) $ 0) 0)
 test2 = TestCase (assertEqual "" (unComp (f <> g) $ 1) 1)
 test3 = TestCase (assertEqual "" (unComp (f <> f) $ 1) 3)
 test4 = TestCase (assertEqual "" (unComp (g <> f) $ 1) 1)
+
+test5 = TestCase (assertEqual ""  (unComp (mappend f mempty) $ 1) (Sum 2))
+
    
 type CompAssoc = Comp Int -> Comp Int -> Comp Int -> Bool
 
 main :: IO ()
 main =  do
 --  quickCheck (semigroupAssoc :: CombineAssoc)
-  counts <- runTestTT (TestList [test1, test2, test3, test4])
+  counts <- runTestTT (TestList [test1, test2, test3, test4, test5])
   putStrLn(show counts)
   return ()
---
+++ HaskellProgrammingFromFirstPrinciples/Chapter15.hsproj/Monoid8.hs
@@ -0,0 +1,45 @@
+module Monoid8 where
+  
+import Data.Monoid
+import Test.QuickCheck
+import SemiGroupAssociativeLaw
+import Test.HUnit
+import MonoidLaws
+
+newtype Mem s a = Mem { runMem :: s -> (a,s) }
+
+mappendMem :: Monoid a => Mem s a -> Mem s a -> Mem s a
+mappendMem f1 f2 =
+  Mem $ combiningFunc 
+    where
+      combiningFunc s =
+        let 
+          (a1, s1) = (runMem f1) s
+          (a2, s2) = (runMem f2) s1
+        in
+          (a1 `mappend` a2, s2)
+    
+instance Monoid a => Monoid (Mem s a)  where
+  mempty = Mem $ \n -> (mempty, n) 
+  mappend = mappendMem
+    
+-- think this link could help: http://stackoverflow.com/questions/16214093/how-to-generate-random-typed-functions
+--instance (Coarbitrary a, Arbitrary b) => Arbitrary (a -> b) where
+--    arbitrary = ...
+   
+f' = Mem $ \s -> ("hi", s + 1)
+
+test1 = TestCase (assertEqual ""  (runMem (f' <> mempty) $ 0) ("hi", 1))
+test2 = TestCase (assertEqual "" (runMem (mempty <> f') 0) ("hi", 1))
+test3 = TestCase (assertEqual "" (runMem mempty 0 :: (String, Int)) ("", 0))
+test4 = TestCase (assertEqual "" (runMem (f' <> mempty) 0) (runMem f' 0))
+test5 = TestCase (assertEqual "" (runMem (mempty <> f') 0) (runMem f' 0))
+
+type MemAssoc = Mem Int (Sum Int) -> Mem Int (Sum Int) -> Mem Int (Sum Int) -> Bool
+
+main :: IO ()
+main =  do
+--  quickCheck (semigroupAssoc :: MemAssoc)
+  counts <- runTestTT (TestList [test1, test2, test3, test4, test5])
+  putStrLn(show counts)
+  return ()
