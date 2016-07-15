d1094699ad7bc0bc71e8635a947317201d5dd0df
Nick Ager <nick.ager@gmail.com>
2016-07-15 17:01:29 +0100

    Chapter 17 WIP

---
+++ HaskellProgrammingFromFirstPrinciples/Chapter17.hsproj/Constant.hs
@@ -0,0 +1,16 @@
+module Constant where
+  
+import Control.Applicative
+import Data.Monoid
+
+newtype Constant a b =
+  Constant { getConstant :: a }
+  deriving (Eq, Ord, Show)
+  
+instance Functor (Constant a) where
+  fmap _ (Constant a) = (Constant a)
+  
+instance Monoid a => Applicative (Constant a) where
+  pure _ = Constant mempty
+  (<*>) (Constant a) (Constant a') = Constant (a <> a')
+
--- HaskellProgrammingFromFirstPrinciples/Chapter17.hsproj/Identity.hs
+++ HaskellProgrammingFromFirstPrinciples/Chapter17.hsproj/Identity.hs
@@ -5,3 +5,10 @@ import Control.Applicative
 newtype Identity a = Identity a
   deriving (Eq, Ord, Show)
   
+instance Functor Identity where
+  fmap f (Identity a) = Identity (f a)
+  
+instance Applicative Identity where
+   pure a = Identity a
+   (<*>) (Identity f) (Identity a) = Identity (f a)
+   
