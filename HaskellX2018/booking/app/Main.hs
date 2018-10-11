b300d670147ae3cfda8359cc5695dac215b22dff
Nick Ager <nick.ager@gmail.com>
2018-10-11 12:14:43 +0100

    WIP

--- HaskellX2018/booking/app/Main.hs
+++ HaskellX2018/booking/app/Main.hs
@@ -5,25 +5,39 @@ import Lib
 import qualified Servant as S
 import Servant ( (:>), (:<|>)(..) )
 import qualified Network.Wai.Handler.Warp as W
+import Control.Concurrent.STM as STM
+import Control.Monad.IO.Class (liftIO)
 
 main :: IO ()
 main = do
     putStrLn "booking system startup"
-    W.run 8080 app
+    db <- mkEmptyBookingDB
+    addFakeBookings db
+    W.run 8080 (app db)
 
-app = S.serve api server
+app db = S.serve api (server db)
 
-api :: S.Proxy (PingAPI :<|> PongAPI)
+api :: S.Proxy (PingAPI :<|> PongAPI :<|> GetBookingTextAPI)
 api = S.Proxy
 
-server = handlePing :<|> handlePong
+server db = handlePing :<|> handlePong :<|> (handleBookingsText db)
 
 type PingAPI = "ping" :> S.Get '[S.PlainText] String
 type PongAPI = "pong" :> S.Get '[S.PlainText] String
+type GetBookingTextAPI = "bookings.txt" :> S.Get '[S.PlainText] String
 
+handleBookingsText db = liftIO $ do
+    v <- STM.atomically $ STM.readTVar db
+    return (show v)
 
 handlePing :: S.Handler String
 handlePing = return "Hello World !!!"
 
 handlePong :: S.Handler String
 handlePong = return "this is pong"
+
+addFakeBookings db = do
+    let (Right r1) = mkBooking 9 12 "first"
+    let (Right r2) = mkBooking 21 24 "second"
+    addBooking db r1
+    addBooking db r2
--- HaskellX2018/booking/src/Lib.hs
+++ HaskellX2018/booking/src/Lib.hs
@@ -2,12 +2,15 @@ module Lib
     ( someFunc
     , Booking
     , mkBooking
+    , overlaps
+    , consBooking
+    , mkEmptyBookingDB
+    , addBooking
     ) where
 
 import qualified Data.Text as T
 import Control.Monad (when)
-
-
+import Control.Concurrent.STM as STM
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
@@ -25,3 +28,26 @@ mkBooking s e d = do
     when (s < 0 ) $ Left "start is too early"
     when (d == "") $ Left "you must give a description"
     return (Booking { _start = s, _end = e, _description = d }) 
+
+overlaps :: Booking -> Booking -> Bool
+overlaps (Booking ls le ld) (Booking rs re rd) = ls < re && rs < le
+
+consBooking :: Booking -> [Booking] -> Either String [Booking]
+consBooking b bs =
+    if or (map (overlaps b) bs)
+    then Left "new booking overlaps existing booking"
+    else return (b : bs)
+
+mkEmptyBookingDB :: IO (STM.TVar [Booking])
+mkEmptyBookingDB = STM.atomically $ STM.newTVar [] :: IO (STM.TVar [Booking])
+
+addBooking :: STM.TVar [Booking] -> Booking -> IO (Either String Int)
+addBooking db booking = STM.atomically $ do
+    oldBookings <- STM.readTVar db
+    let newBookings = booking `consBooking` oldBookings
+    case newBookings of
+        Left err -> return (Left err)
+        Right b' -> do
+            STM.writeTVar db b'
+            pure (Right (_start booking))
+
